d744e4f27e66916731262e7b93dedbf8a98716c8
John Doe~ <johndoe@example.com>
2024-10-10 00:40:02 +0800

    feat: hallo

--- src/pages/navbar.tsx
+++ src/pages/navbar.tsx
@@ -107,7 +107,7 @@ const Navbar = () => {
             </ul>
           </div>
           <div className="gap-2 sm:gap-3 md:gap-4 hidden md:flex ml-12">
-            <Button className="bg-black/90 text-white/90 py-1 px-1 sm:py-1 sm:px-4 rounded-full hover:bg-green-100/90 transition-colors border-2 border-black/90 hover:text-black/90 font-mono">
+            <Button className="bg-black/90 text-white/90 py-1 px-1 sm:py-1 sm:px-4 rounded-full hover:bg-white transition-colors border-2 border-black/90 hover:text-black/90 font-mono">
               Contract <AiOutlineWhatsApp className="ml-1" />
             </Button>
           </div>
@@ -231,7 +231,7 @@ const Navbar = () => {
                     alt="NextUI Album Cover"
                     className="rounded-xl shadow-2xl md:w-[1400px] md:h-[800px] m-0 p-0"
                   />
-                  <CardFooter className="justify-between before:bg-green-100/10 border-green-200/20 border overflow-hidden py-0 absolute before:rounded-none rounded-none bottom-0 shadow-small m-0 z-10 w-full h-20">
+                  <CardFooter className="justify-between before:bg-green-100/10 bg-green-900/10 border-green-200/20 border overflow-hidden py-0 absolute before:rounded-none rounded-none bottom-0 shadow-small m-0 z-10 w-full h-20">
                     <p className="text-3xl text-white/90">
                       I wanna intership Contact me
                     </p>
